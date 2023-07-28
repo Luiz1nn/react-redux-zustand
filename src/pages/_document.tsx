2cdab25f380854bc393a8e16e6e583d8fb2a184e
Luis Fernando <luisfernandobr2003@gmail.com>
2023-07-28 11:51:25 -0300

    feat(store): added TodoStore

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>React + Redux + Zustand</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
