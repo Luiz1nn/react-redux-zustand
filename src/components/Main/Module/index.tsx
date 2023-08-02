0a8fa2004e0dafc2be606ca472075ba64bf6348e
Luis Fernando <luisfernandobr2003@gmail.com>
2023-08-02 14:38:40 -0300

    feat(Module): added radix collapsible

--- src/components/Main/Module/index.tsx
+++ src/components/Main/Module/index.tsx
@@ -1,29 +1,38 @@
 import { ChevronDown } from 'lucide-react'
 import { Lesson } from '../Lesson'
+import * as Collapsible from '@radix-ui/react-collapsible'
 
-export function Module() {
+type Props = {
+  moduleIndex: number
+  title: string
+  amountOfLessons: number
+}
+
+export function Module({ moduleIndex, title, amountOfLessons }: Props) {
   return (
-    <div>
-      <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+    <Collapsible.Root className="group">
+      <Collapsible.Trigger className="flex w-full items-center gap-3 bg-zinc-800 p-4">
         <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-xs">
-          1
+          {moduleIndex + 1}
         </div>
 
         <div className="flex flex-col gap-1 text-left">
-          <strong className="text-sm">Desvendando o Redux</strong>
-          <span className="text-xs text-zinc-400">12 aulas</span>
+          <strong className="text-sm">{title}</strong>
+          <span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
         </div>
 
-        <ChevronDown className="w-5 h-5 ml-auto text-zinc-400" />
-      </button>
+        <ChevronDown className="w-5 h-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform duration-300" />
+      </Collapsible.Trigger>
 
-      <nav className="relative flex flex-col gap-4 p-6">
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-      </nav>
-    </div>
+      <Collapsible.Content>
+        <nav className="relative flex flex-col gap-4 p-6">
+          <Lesson title="Fundamentos do Redux" duration="09:13" />
+          <Lesson title="Fundamentos do Redux" duration="09:13" />
+          <Lesson title="Fundamentos do Redux" duration="09:13" />
+          <Lesson title="Fundamentos do Redux" duration="09:13" />
+          <Lesson title="Fundamentos do Redux" duration="09:13" />
+        </nav>
+      </Collapsible.Content>
+    </Collapsible.Root>
   )
 }
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -6,9 +6,22 @@ export function Main() {
     <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-800 shadow pr-80">
       <Video />
 
-      <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-auto scrollbar scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-        <Module />
-        <Module />
+      <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-auto scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
+        <Module
+          moduleIndex={0}
+          title="Desvendando o Redux"
+          amountOfLessons={3}
+        />
+        <Module
+          moduleIndex={1}
+          title="Desvendando o Redux"
+          amountOfLessons={3}
+        />
+        <Module
+          moduleIndex={2}
+          title="Desvendando o Redux"
+          amountOfLessons={3}
+        />
       </aside>
     </main>
   )
