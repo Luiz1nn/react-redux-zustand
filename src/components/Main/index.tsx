e901265b4a8c04342aa7fba7eb1bdbaa38e3ff0a
Luis Fernando <luisfernandobr2003@gmail.com>
2023-08-02 14:53:31 -0300

    feat(Module): use player store

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,27 +1,23 @@
+import { useAppSelector } from '~/store'
 import { Module } from './Module'
 import { Video } from './Video'
 
 export function Main() {
+  const modules = useAppSelector(({ player }) => player.course.modules)
+
   return (
     <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-800 shadow pr-80">
       <Video />
 
       <aside className="w-80 absolute top-0 bottom-0 right-0 border-l divide-y-2 divide-zinc-900 border-zinc-800 bg-zinc-900 overflow-y-auto scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
-        <Module
-          moduleIndex={0}
-          title="Desvendando o Redux"
-          amountOfLessons={3}
-        />
-        <Module
-          moduleIndex={1}
-          title="Desvendando o Redux"
-          amountOfLessons={3}
-        />
-        <Module
-          moduleIndex={2}
-          title="Desvendando o Redux"
-          amountOfLessons={3}
-        />
+        {modules.map(({ id, lessons, title }, index) => (
+          <Module
+            key={id}
+            moduleIndex={index}
+            title={title}
+            amountOfLessons={lessons.length}
+          />
+        ))}
       </aside>
     </main>
   )
