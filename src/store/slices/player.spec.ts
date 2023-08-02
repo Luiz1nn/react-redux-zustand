98023450796a529471877bacbe4dc5d726101445
Luis Fernando <luisfernandobr2003@gmail.com>
2023-08-02 17:36:14 -0300

    test: should be able to play next video automatically

--- src/store/slices/player.spec.ts
+++ src/store/slices/player.spec.ts
@@ -1,16 +1,53 @@
 import { describe, expect, it } from 'vitest'
-import { player as reducer, playerSlice, play } from './player'
+import { player as reducer, play, next } from './player'
 
 describe('player slice', () => {
-  it('should be able to play', () => {
-    const initialState = playerSlice.getInitialState()
+  const exampleState = {
+    course: {
+      modules: [
+        {
+          id: '1',
+          title: 'Iniciando com React',
+          lessons: [
+            { id: 'Jai8w6K_GnY', title: 'CSS Modules', duration: '13:45' },
+            { id: 'Pj8dPeameYo', title: 'CSS Global', duration: '03:23' },
+          ],
+        },
+        {
+          id: '2',
+          title: 'Estrutura da aplicação',
+          lessons: [
+            {
+              id: 'gE48FQXRZ_o',
+              title: 'Componente: Comment',
+              duration: '13:45',
+            },
+            { id: 'Ng_Vk4tBl0g', title: 'Responsividade', duration: '10:05' },
+          ],
+        },
+      ],
+    },
+    currentModuleIndex: 0,
+    currentLessonIndex: 0,
+  }
 
+  it('should be able to play', () => {
     const { currentModuleIndex, currentLessonIndex } = reducer(
-      initialState,
+      exampleState,
       play([1, 2]),
     )
 
     expect(currentModuleIndex).toEqual(1)
     expect(currentLessonIndex).toEqual(2)
+  })
+
+  it('should be able to play next video automatically', () => {
+    const { currentModuleIndex, currentLessonIndex } = reducer(
+      exampleState,
+      next(),
+    )
+
+    expect(currentModuleIndex).toEqual(0)
+    expect(currentLessonIndex).toEqual(1)
   })
 })
